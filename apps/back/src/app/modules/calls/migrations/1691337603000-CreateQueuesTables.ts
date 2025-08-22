69a6be95948ed7988bf106db704d361331e51d73
Jamshed Latipov <jamshedlatipov@gmail.com>
2025-08-22 22:41:02 +0500

    fix:

--- apps/back/src/app/modules/calls/migrations/1691337603000-CreateQueuesTables.ts
+++ apps/back/src/app/modules/calls/migrations/1691337603000-CreateQueuesTables.ts
@@ -8,12 +8,12 @@ export class CreateQueuesTables1691337603000 implements MigrationInterface {
   SELECT 'support', 'Support queue for testing', 'default', 'ringall', 'default', 0, 15, 0, 0, 0, true, true, false
   WHERE NOT EXISTS (SELECT 1 FROM queues WHERE name = 'support');
 
-  INSERT INTO queue_members (queue_name, member_name, penalty, member_type, paused, memberid, interface)
-  SELECT 'support', 'PJSIP/operator1', 0, 'pjsip', false, 'operator1', 'PJSIP/operator1'
+  INSERT INTO queue_members (queue_name, member_name, penalty, member_type, paused, memberid, interface, uniqueid)
+  SELECT 'support', 'PJSIP/operator1', 0, 'pjsip', false, 'operator1', 'PJSIP/operator1', 'PJSIP/operator1'
   WHERE NOT EXISTS (SELECT 1 FROM queue_members WHERE queue_name = 'support' AND interface = 'PJSIP/operator1');
 
-  INSERT INTO queue_members (queue_name, member_name, penalty, member_type, paused, memberid, interface)
-  SELECT 'support', 'PJSIP/operator2', 0, 'pjsip', false, 'operator2', 'PJSIP/operator2'
+  INSERT INTO queue_members (queue_name, member_name, penalty, member_type, paused, memberid, interface, uniqueid)
+  SELECT 'support', 'PJSIP/operator2', 0, 'pjsip', false, 'operator2', 'PJSIP/operator2', 'PJSIP/operator2'
   WHERE NOT EXISTS (SELECT 1 FROM queue_members WHERE queue_name = 'support' AND interface = 'PJSIP/operator2');
     `);
   }
