903adfd36ba6a192f02728af8d5746a0d28f26ce
Jamshed Latipov <jamshedlatipov@gmail.com>
2025-12-14 01:41:36 +0500

    Refactor code structure for improved readability and maintainability

--- apps/back/src/app/app.module.ts
+++ apps/back/src/app/app.module.ts
@@ -54,7 +54,7 @@ import { INTEGRATIONS_MIGRATIONS } from './modules/integrations/migrations';
         ...USER_ACTIVITY_MIGRATIONS,
         ...INTEGRATIONS_MIGRATIONS,
       ],
-      migrationsRun: false,
+      migrationsRun: true,
     }),
     ClientsModule.register([
       {
