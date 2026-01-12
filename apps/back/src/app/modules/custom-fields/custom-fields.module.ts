c401fcf6b4bb122afa8c8eebcbd7c203a3e502a1
Jamshed Latipov <jamshedlatipov@gmail.com>
2026-01-12 12:08:57 +0500

    feat: Add UserModule to CustomFieldsModule imports

--- apps/back/src/app/modules/custom-fields/custom-fields.module.ts
+++ apps/back/src/app/modules/custom-fields/custom-fields.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomFieldDefinition } from './entities/custom-field-definition.entity';
 import { CustomFieldsService } from './services/custom-fields.service';
 import { CustomFieldsController } from './controllers/custom-fields.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CustomFieldDefinition])],
+  imports: [TypeOrmModule.forFeature([CustomFieldDefinition]), UserModule],
   controllers: [CustomFieldsController],
   providers: [CustomFieldsService],
   exports: [CustomFieldsService],
